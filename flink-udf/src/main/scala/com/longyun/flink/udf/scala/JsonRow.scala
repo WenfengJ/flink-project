38b4bf08ebe792e2077d4b55b3b7b4fa982e8990
lynnyuan <yuanxiaolong@inspur.com>
2019-02-19 10:26:14 +0800

    JsonRow function test

---
+++ flink-udf/src/main/scala/com/longyun/flink/udf/scala/JsonRow.scala
@@ -0,0 +1,56 @@
+package com.longyun.flink.udf.scala
+
+import java.sql.Timestamp
+
+import org.apache.flink.table.api.functions.TableFunction
+import org.apache.flink.table.api.types.{DataType, DataTypes, RowType}
+import org.apache.flink.table.runtime.functions.utils.JsonUtils
+import org.apache.flink.types.Row
+
+import scala.annotation.varargs
+import scala.collection.mutable.ArrayBuffer
+
+class JsonRow(val fieldTypeMap:Map[String, DataType]) extends TableFunction[Row] {
+
+  @varargs
+  def eval(jsonStr: String, paths: String*): Unit = {
+    println("jsonStr="+jsonStr)
+    val row = new Row(paths.length)
+    for (i <- 0 until paths.length) {
+      val typeName = fieldTypeMap.getOrElse(paths(i), DataTypes.STRING)
+      val value = JsonUtils.getInstance.getJsonObject(jsonStr, "$."+paths(i))
+      typeName match {
+        case DataTypes.TIMESTAMP => {
+          val v = java.lang.Long.parseLong(value)
+          val _value = new Timestamp(v)
+          row.setField(i, _value)
+        }
+        case DataTypes.FLOAT => row.setField(i, java.lang.Float.parseFloat(value))
+        case DataTypes.INT => row.setField(i, java.lang.Integer.parseInt(value))
+        case _ => row.setField(i, value)
+
+      }
+
+      println(i + " : " + value)
+    }
+
+    collect(row)
+  }
+
+  override def getResultType(arguments: Array[AnyRef], argTypes: Array[Class[_]]): DataType = {
+    import util.control.Breaks._
+    val fields = new ArrayBuffer[String]()
+    val types = new ArrayBuffer[DataType]()
+    for(i <- 0 until argTypes.length){
+      breakable{
+        if(arguments(i) == null) {
+          break
+        }
+        fields += (arguments(i).toString)
+        types += fieldTypeMap.getOrElse((arguments(i).toString), DataTypes.STRING)
+      }
+    }
+
+    new RowType(types.toArray, fields.toArray)
+  }
+}
